10585a52529fe3f9b01fb03d190c5f356890c263
cfkelly18 <cfkelly18@pm.me>
2023-04-18 18:17:18 -0600

    intial working fn visitor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod analysis; 
+mod utils;
+mod driver;
+
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
-    println!("Hello, world!");
+    // Get the command-line arguments passed to the program
+    // let args: Vec<String> = env::args().collect();
+    //TODO - get the path to the project
+    let dir = PathBuf::from("/Users/cfkelly18/DEV/cosmwasm/cw-plus");
+
+    let analyzer = analysis::Analyzer::new();
+    // Call your analysis function from the analysis module and pass the arguments to it
+    //analyzer.analyze(&args);
 }
